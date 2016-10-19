c9aaa264447da03ca3cb63ca3d09fc82f97eb6e3
karasik <dkostin2000@mail.ru>
2016-10-19 17:25:22 +0300

    double describe

--- test/test.js
+++ test/test.js
@@ -1,4 +1,14 @@
 describe("pow", function() {
+    
+   describe("Check main functional", function() {
+    
+   before( function() {
+       console.log("я уже 2 часа бьюсь с этими тестами");
+   })
+   
+   after( function() {
+       console.log("и вот наконец-то закончил")
+   }) 
    function makeTest(x) {
        var expected = x * x * x;
        it("возведение " + x + "в степень 3 должно давать значение " + expected, function() {
@@ -9,4 +19,5 @@ describe("pow", function() {
     for (var x = 1; x <= 12; x++) {
         makeTest(x);
     }
+   });
 });
